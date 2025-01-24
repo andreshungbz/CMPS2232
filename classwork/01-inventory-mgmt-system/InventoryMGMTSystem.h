fde86db3e1e9638f7b16f8bb40e4d5f791712ebf
Andres Hung <andres.hung@outlook.com>
2025-01-24 12:30:57 -0600

    Update

--- classwork/01-inventory-mgmt-system/InventoryMGMTSystem.h
+++ classwork/01-inventory-mgmt-system/InventoryMGMTSystem.h
@@ -1,6 +1,34 @@
-#ifndef INVENTORYMGMTSYSTEM_H
-#define INVENTORYMGMTSYSTEM_H
+#ifndef INVENTORY_MGMT_SYSTEM_H
+#define INVENTORY_MGMT_SYSTEM_H
 
+#include <string>
+#include <unordered_map>
+#include "Inventory.h"
 
+class InventoryMGMTSystem {
+public:
+    // constructor
+    explicit InventoryMGMTSystem(const std::string& name);
+    // destructor
+    ~InventoryMGMTSystem();
 
-#endif // INVENTORYMGMTSYSTEM_H
+    // getters
+    std::string getName() const { return m_name; };
+    int getInventoryCount() const { return m_inventoryCount; };
+
+    // setters
+    void setName(const std::string& name);
+
+    // other
+    void addInventory(const std::string& name);
+    bool removeInventory(const std::string& name);
+    void renameInventory(const std::string& name);
+    void display() const;
+private:
+    std::string m_name;
+    std::unordered_map<std::string, Inventory> m_inventories;
+    int m_inventoryCount{0};
+    int m_nextID{1};
+};
+
+#endif // INVENTORY_MGMT_SYSTEM_H
